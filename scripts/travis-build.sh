901dcbaf6af1aa6ee88dfc5ff723c712b4284a5d
Nicolas F Rouquette <nicolas.f.rouquette@jpl.nasa.gov>
2016-08-30 21:41:29 +0000

    travis build xor deploy

--- scripts/travis-build.sh
+++ scripts/travis-build.sh
@@ -2,5 +2,9 @@
 
 set -ev
 
+# Build if TRAVIS_TAG is unset or empty.
+
+[ -n "${TRAVIS_TAG}"" ] && exit 0;
+
 sbt -jvm-opts travis/jvmopts.compile artifacts makeSite
 
--- scripts/travis-deploy.sh
+++ scripts/travis-deploy.sh
@@ -2,8 +2,10 @@
 
 set -ev
 
+# Deploy if TRAVIS_SECURE_ENV_VARS is true and TRAVIS_TAG is set
+
 [ ${TRAVIS_SECURE_ENV_VARS} == false ] && exit 0;
-[ -z ${TRAVIS_TAG} ] && exit 0;
+[ -z "${TRAVIS_TAG}"" ] && exit 0;
 
 sbt -jvm-opts travis/jvmopts.compile -Dproject.version=$TRAVIS_TAG publishSigned ghpagesPushSite
 
