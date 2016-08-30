aa2793b04fc105ec49e410b8422846058f7fc3fa
Nicolas F Rouquette <nicolas.f.rouquette@jpl.nasa.gov>
2016-08-30 21:43:52 +0000

    travis build xor deploy

--- scripts/travis-build.sh
+++ scripts/travis-build.sh
@@ -4,7 +4,7 @@ set -ev
 
 # Build if TRAVIS_TAG is unset or empty.
 
-[ -n "${TRAVIS_TAG}"" ] && exit 0;
+[ -n "${TRAVIS_TAG}" ] && exit 0;
 
 sbt -jvm-opts travis/jvmopts.compile artifacts makeSite
 
