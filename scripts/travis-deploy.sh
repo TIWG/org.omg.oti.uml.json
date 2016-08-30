0635e861687bc599e931a7574fcfa3b40712b071
Nicolas F Rouquette <nicolas.f.rouquette@jpl.nasa.gov>
2016-08-30 21:35:24 +0000

    travis lint

--- scripts/travis-deploy.sh
+++ scripts/travis-deploy.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 set -ev
+
+[ ${TRAVIS_SECURE_ENV_VARS} == false ] && exit 0;
+[ -z ${TRAVIS_TAG} ] && exit 0;
+
 sbt -jvm-opts travis/jvmopts.compile -Dproject.version=$TRAVIS_TAG publishSigned ghpagesPushSite
 
